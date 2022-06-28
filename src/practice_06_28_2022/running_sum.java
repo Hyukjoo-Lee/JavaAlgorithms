6eebe162e7e31c6696e0c6ac8e5317c5dd14dcd9
Jaydon Lee <96518885+Hyukjoo-Lee@users.noreply.github.com>
2022-06-28 11:20:58 -0700

    06-28

--- src/practice_06_28_2022/running_sum.java
+++ src/practice_06_28_2022/running_sum.java
@@ -1,10 +1,38 @@
 package practice_06_28_2022;
-
+/**
+ * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+ * Return the running sum of nums.
+ * @author "HyukjooLee"
+ */
 public class running_sum {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Test
+		int[] arr = {1,2,3,4};
+		runningSum(arr);
+	}
+	
+	public static int[] runningSum(int[] nums) {
+		int[] ans = new int[nums.length];
+		// ans[0] = nums[0];
+		// ans[1] = nums[0] + nums[1]
+		// ans[2] = nums[0] + nums[1] + nums[2]...
+		
+		for (int i = 0; i < ans.length; i++) {
+			if(i == 0) {
+				ans[i] = nums[i];
+			} else {
+				ans[i] = ans[i-1] + nums[i];
+			}
+		}
+		
+		// Print the array to be returned
+		for (int i = 0; i < ans.length; i++) {
+			System.out.print(ans[i] + " ");
+		}
+		
+		return nums;
 	}
 
 }
+
